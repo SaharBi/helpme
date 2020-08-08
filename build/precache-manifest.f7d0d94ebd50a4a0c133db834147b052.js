1145f72341b8942564c2aacf53a3498dd4359ab2
Sahar Bitton <saharbi@walla.co.il>
2020-08-08 11:38:50 +0300

    second

---
+++ build/precache-manifest.f7d0d94ebd50a4a0c133db834147b052.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434138525f1e57668de438e4b7873e54",
+    "url": "/helpme/index.html"
+  },
+  {
+    "revision": "c9d2f24237b148481bef",
+    "url": "/helpme/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3e6b7e96903930c1df92",
+    "url": "/helpme/static/css/main.dc39e95a.chunk.css"
+  },
+  {
+    "revision": "c9d2f24237b148481bef",
+    "url": "/helpme/static/js/2.42337814.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/helpme/static/js/2.42337814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6b7e96903930c1df92",
+    "url": "/helpme/static/js/main.33330668.chunk.js"
+  },
+  {
+    "revision": "024de05e16c604ec55c5",
+    "url": "/helpme/static/js/runtime-main.cc62f2fe.js"
+  }
+]);
